3bfcbc0a33a0fa03d5bbc931e713bb6e87abe901
Sabariy <123258023+sabariyy@users.noreply.github.com>
2025-01-22 18:41:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || "Fenix~diroyaxiro",
+  SESSION_ID:process.env.SESSION_ID || "Fenix~ebiwureqaf",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[.]',
   BRANCH: "main",
